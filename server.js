2cd6b79f41b3838eeb4a71245cd9b29fe435fea0
akash kumar <akash@arivani.net>
2024-04-24 11:29:17 +0530

    initial comit for github

---
+++ server.js
@@ -0,0 +1,69 @@
+import express from "express";
+import axios from "axios";
+import { createClient } from "redis";
+
+const app = express();
+const port = 3000;
+
+let redisClient;
+
+redisClient = await createClient()
+  .on('error', err => console.log('Redis Client Error', err))
+  .connect();
+
+async function fetchApiData(species) {
+  const apiResponse = await axios.get(
+    `https://www.fishwatch.gov/api/species/${species}`
+  );
+  console.log("Request sent to the API");
+  return apiResponse.data;
+}
+
+async function cacheData(req, res, next) {
+  const species = req.params.species;
+  let results;
+  try {
+    const cacheResults = await redisClient.get(species);
+    if (cacheResults) {
+      results = JSON.parse(cacheResults);
+      res.send({
+        fromCache: true,
+        data: results,
+      });
+    } else {
+      next();
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(404);
+  }
+}
+async function getSpeciesData(req, res) {
+  const species = req.params.species;
+  let results;
+
+  try {
+    results = await fetchApiData(species);
+    if (results.length === 0) {
+      throw "API returned an empty array";
+    }
+    await redisClient.set(species, JSON.stringify(results), {
+      EX: 180,
+      NX: true,
+    });
+
+    res.send({
+      fromCache: false,
+      data: results,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(404).send("Data unavailable");
+  }
+}
+
+app.get("/fish/:species", cacheData, getSpeciesData);
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
